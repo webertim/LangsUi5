db8497b34fd8c626df6755c2a8a8268356e28b99
Flogo345 <florianweber1998@web.de>
2020-05-12 11:21:13 +0000

    mainpage visuals finished

--- Quickstart/webapp/controller/ProgrammingLanguagesList.controller.js
+++ Quickstart/webapp/controller/ProgrammingLanguagesList.controller.js
@@ -14,19 +14,51 @@ sap.ui.define([
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
+			
+			var oTable = this.getView().byId("ProgrammingLanguagesTable");
+			oTable.bindItems({
+				path: 'programmingLanguage>/ProgrammingLanguages',
+				template: new sap.m.ColumnListItem({
+					cells: [
+						new sap.m.Text({
+							text: "{programmingLanguage>Name}"
+						}),
+						new sap.m.Text({
+							text: "{programmingLanguage>OperationArea}"
+						}),
+						new sap.m.Text({
+							text: "{programmingLanguage>Release}"
+						}),
+						new sap.m.Text({
+							text: "{programmingLanguage>Popularity}"
+						}),
+						new sap.m.Text({
+							text: "{programmingLanguage>GettingStarted}"
+						}),
+						new sap.m.Text({
+							text: "{programmingLanguage>Documentation}"
+						}),
+						new sap.m.RatingIndicator("noIconSize", {
+							enabled: false,
+							value: "{programmingLanguage>Rating}",
+							tooltip: "This is a tooltip"
+						})
+						]
+				})
+			});
 		},
-		onFilterInvoices : function (oEvent) {
+		onFilterProgrammingLanguages : function (oEvent) {
 
 			// build filter array
 			var aFilter = [];
 			var sQuery = oEvent.getParameter("query");
 			if (sQuery) {
-				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+				aFilter.push(new Filter("Name", FilterOperator.Contains, sQuery));
 			}
 
 			// filter binding
-			var oList = this.byId("invoiceList");
-			var oBinding = oList.getBinding("items");
+			var oTable = this.byId("ProgrammingLanguagesTable");
+			var oBinding = oTable.getBinding("items");
 			oBinding.filter(aFilter);
 		}
 
